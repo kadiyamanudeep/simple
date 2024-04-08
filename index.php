1bf09b9adcf817559fb9f3e10b5f43120648a37a
anudeep <anudeepkadiyam@gmail.com>
2024-04-08 13:26:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head><title>hello world script</title></head>
+    <body>
+    <?php 
+    $a = 9;
+    $b = "9";
+    // $c = $a && $b;
+    // $d = $a || $b;
+    // $e = $b && $c;
+    // $f = $a || $d;
+    
+    // $logic = array( $a, $b, $c, $d, $e, $f, );
+    // echo '<pre>';
+    // var_dump($logic);
+    // echo'</pre>';
+
+    if ($a > $b){
+        echo "$a is greater than $b";
+    } else if($a < $b) {
+        echo "$a is not greater than $b";
+    }else if ($a !== $b){
+        echo "$a is equal to $b";
+    }
+
+
+
+
+
+    //php ends here
+    ?> 
+
+    </body>
+</html>  
