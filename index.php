eec682c94d3f1a48441a1784b1540d68f6a5bed1
Anudeep <anudeepkadiyam@gmail.com>
2024-05-03 11:53:10 +0530

    class lesson completed

--- index.php
+++ index.php
@@ -12,22 +12,90 @@
     //  *            Classes              *
     //  ***********************************
 
-    class Customer
-    {
-        private $name;
-        public function setName($name)
-        {
-            $this->name = $name;
-        }
-        public function getName()
-        {
-            return $this->name;
-        }
-    }
 
-    $customer = new Customer();
-    $customer->setName('anudeep');
-    echo $customer->getName();
+    // class Person
+    // {
+    //     var $first_name;
+    //     var $last_name;
+
+    //     function __construct($fn, $ln)
+    //     {
+    //         $this->first_name = $fn;
+    //         $this->last_name = $ln;
+    //     }
+
+    //     public function get_first_name()
+    //     {
+    //         return $this->first_name;
+    //     }
+
+    //     public function get_last_name()
+    //     {
+    //         return $this->last_name;
+    //     }
+    // }
+
+    // Challenge: Sort this array of Person objects by last, then first name! 
+
+    // $rob = new Person('Rob', 'Casabona');
+    // $joe = new Person('Joe', 'Casabona');
+    // $erin = new Person('Erin', 'Casabona');
+    // $steve = new Person('Steve', 'Wozniack');
+    // $bill = new Person('Bill', 'Gates');
+    // $walt = new Person('Walt', 'Disney');
+    // $bob = new Person('Bob', 'Iger');
+
+    // $people = array($rob, $joe, $erin, $steve, $bill, $walt, $bob);
+
+    // usort($people, function ($a, $b) {
+    //     return [$a->get_last_name(), $a->get_first_name()] <=> [$b->get_last_name(), $b->get_first_name()];
+    // });
+
+    // echo '<pre>';
+    // print_r($people);
+    // echo '</pre>';
+
+    // Constructors
+    // class BankAccount
+    // {
+
+    //     function __construct(private $accountNumber, private $balance)
+    //     {
+    //         $this->accountNumber = $accountNumber;
+    //         $this->balance = $balance;
+    //     }
+    //     function getAccNum()
+    //     {
+    //         return $this->accountNumber;
+    //     }
+    //     function getBal()
+    //     {
+    //         return $this->balance;
+    //     }
+    // }
+
+    // $account = new BankAccount(1, 100);
+    // echo '<p>this is a account Number</p>' . $account->getAccNum();
+    // echo '<br><p>this is a balance</p>' . $account->getBal();
+
+
+
+    // class Customer
+    // {
+    //     private $name;
+    //     public function setName($name)
+    //     {
+    //         $this->name = $name;
+    //     }
+    //     public function getName()
+    //     {
+    //         return $this->name;
+    //     }
+    // }
+
+    // $customer = new Customer();
+    // $customer->setName('anudeep');
+    // echo $customer->getName();
 
     // class Person
     // {
@@ -435,6 +503,7 @@
     //php ends here
 
     ?>
+
 
 </body>
 
